963a91cc96eaebd2098e200b659803e67bb355cf
VishnuVibin <vishnuvibin813@gmail.com>
2026-06-02 21:26:49 +0530

    Time: 61 ms (29.29%) | Memory: 28.6 MB (75.69%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minprice=float('inf')
+        maxprofit=0
+        for i in prices:
+            minprice=min(minprice,i)
+            maxprofit=max(maxprofit,i-minprice)
+        return maxprofit
+
+
+        
