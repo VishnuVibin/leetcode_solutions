a7b274115b202d535131d7c7ef96717e8116f7d0
VishnuVibin <vishnuvibin813@gmail.com>
2026-06-09 10:05:19 +0530

    Time: 35 ms (11.76%) | Memory: 16.5 MB (0.00%) - LeetSync

---
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxTotalValue(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        res=max(nums)-min(nums)
+        return res*k
+        
