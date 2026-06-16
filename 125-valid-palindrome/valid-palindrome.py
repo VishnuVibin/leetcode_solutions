84bcea879be15854b10ef88696438e387c891928
VishnuVibin <vishnuvibin813@gmail.com>
2026-06-16 11:24:40 +0530

    Time: 250 ms (10.27%) | Memory: 13.1 MB (48.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -8,8 +8,6 @@ class Solution(object):
         for i in s:
             if(i.isalnum()):
                 str0+=i.lower()
-        res=True
-        if(str0!=str0[::-1]):
-            res=False
-        return res
+        
+        return str0==str0[::-1]
         
