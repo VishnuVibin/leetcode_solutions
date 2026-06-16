3e8e4cd0fbb6338b10bf069fada49120549034b7
VishnuVibin <vishnuvibin813@gmail.com>
2026-06-16 11:22:01 +0530

    Time: 140 ms (26.24%) | Memory: 13.1 MB (48.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -5,26 +5,16 @@ class Solution(object):
         :rtype: bool
         """
         str0=""
-        str1=[]
         for i in s:
             if(i.isalnum()):
                 str0+=i.lower()
-                str1.append(i.lower())
-
+        res=True
         left=0
         right=len(str0)-1
         while(left<right):
-            temp=str1[right]
-            str1[right]=str1[left]
-            str1[left]=temp
+            if(str0[left]!=str0[right]):
+                res=False
             left+=1
             right-=1
-        str2=""
-        for i in str1:
-            str2+=i
-        if(str0==str2):
-            return True
-        else:
-            return False
-
-        
+        return res
+                
